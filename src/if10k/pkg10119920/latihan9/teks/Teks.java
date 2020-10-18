12e534fc9e4b5aa8fb5c2e4f5f422067e07f4aa2
ursBot <umar.10119920@mahasiswa.ac.id>
2020-10-18 17:17:08 +0700

    upload pertama

---
+++ src/if10k/pkg10119920/latihan9/teks/Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119920.latihan9.teks;
+
+
+/**
+*
+ * @author
+ * NAMA     : Umar Said Adi Pranoto
+ * KELAS    : IF10K
+ * NIM      : 10119920
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+
+public class Teks{
+    
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char : " + tab + "berisi " + c);
+    }
+
+
+}
